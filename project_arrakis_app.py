094bed8f8ddb4ddf9d143a40bb9a4f71691e5def
ilbidi <fbidinotto@gmail.com>
2017-04-21 23:11:50 +0200

    Simple views

--- project_arrakis_app.py
+++ project_arrakis_app.py
@@ -1,11 +1,15 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask and ilbidi!'
+def index():
+    return render_template('credits.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
