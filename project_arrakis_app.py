990e4deb170cba0de070d834efb4093dcb34a72e
ilbidi <fbidinotto@gmail.com>
2017-04-22 10:17:56 +0200

    Login and logout management

--- project_arrakis_app.py
+++ project_arrakis_app.py
@@ -6,10 +6,31 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 
 app = Flask(__name__)
 
+app.config.update(dict(
+    SECRET_KEY='test'
+))
+
 @app.route('/')
 def index():
-    return render_template('credits.html')
+    return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if( request.method=='POST'):
+        if( request.form['username']!='fabio'):
+            error='Invalid username'
+        elif( request.form['password']!='fabio'):
+            error='Invalid password'
+        else:
+            session['logged_in']=True
+            session['username']=request.form['username']
+            flash('Login successfull.')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in')
+    flash('Session ended')
+    return redirect(url_for('index'))
